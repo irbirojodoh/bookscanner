5d25f30c1dad90f7a71e1d0c00542e94ae4da578
Ibrahim Rijal <greget007.ir@gmail.com>
2024-11-09 21:54:50 +0700

    Nyoba masikin camera view, susah :(

--- despro-kel3/HomeView.swift
+++ despro-kel3/HomeView.swift
@@ -8,90 +8,120 @@ struct HomeView: View {
     @State private var isShowingAlert = false  // To trigger the alert
     @State private var bleReceivedValue = ""
     @State private var timer: Timer?
+    @State private var navigateToBleTestView = false  // State for triggering navigation
 
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Image(systemName: "doc.plaintext")
-                .font(.system(size: 80))
-                .foregroundColor(.gray)
-            
-            Text("You don't have any document!")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.top, 16)
-            
-            if let image = capturedImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 300)
-                    .padding()
-            }
-            
-            Text("Scan or add your document by clicking the + button below and save as MEMORIES format")
-                .font(.body)
-                .foregroundColor(.gray)
-                .multilineTextAlignment(.center)
-                .padding(.top, 8)
-                .padding(.horizontal, 40)
-            
-            Spacer()
-            
-            HStack {
+        NavigationView {
+            VStack {
                 Spacer()
                 
-                Button(action: {
-                    if bleManager.isConnected {
-                        isCapturing = true
-                        // Only send the command to BLE if connected
-                        bleManager.writeValue("1")  // Send value "1" to BLE device
-                    } else {
-                        // Show the alert if BLE is not connected
-                        isShowingAlert = true
-                    }
-                }) {
-                    Image(systemName: "plus")
-                        .font(.title)
-                        .foregroundColor(.blue)
+                Image(systemName: "doc.plaintext")
+                    .font(.system(size: 80))
+                    .foregroundColor(.gray)
+                
+                Text("You don't have any document!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.top, 16)
+                
+                if let image = capturedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 300)
                         .padding()
                 }
-                .sheet(isPresented: $isCapturing) {
-                    CameraViewWrapper(capturedImage: $capturedImage, isCapturing: $isCapturing, bleReceivedValue: $bleReceivedValue, bleManager:$bleManager)
-                }
-
-                Spacer()
-            }
-        }
-        .navigationBarTitle("MEMORIES Scanner", displayMode: .inline)
-        .navigationBarItems(trailing:
-            Menu {
-                Button(action: {
-                    bleManager.isConnected ? bleManager.disconnect() : bleManager.connect()
-                }) {
-                    Text(bleManager.isConnected ? "Disconnect" : "Connect")
-                        .frame(maxWidth: .infinity)
-                        .font(.headline.weight(.semibold))
-                }
-                .padding()
-                .background(bleManager.isConnected ? Color.red : Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8)
-            } label: {
-                Image(systemName: "ellipsis")
-                    .font(.title2)
+                
+                Text("Scan or add your document by clicking the + button below and save as MEMORIES format")
+                    .font(.body)
                     .foregroundColor(.gray)
-            })
-            .onAppear {
-                startBLEScanTimer()  // Start the BLE scan when the view appears
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 8)
+                    .padding(.horizontal, 40)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        if bleManager.isConnected {
+                            isCapturing = true
+                            // Only send the command to BLE if connected
+                            bleManager.writeValue("1")  // Send value "1" to BLE device
+                        } else {
+                            // Show the alert if BLE is not connected
+                            isShowingAlert = true
+                        }
+                    }) {
+                        Image(systemName: "plus")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
+                    .sheet(isPresented: $isCapturing) {
+                        CameraViewWrapper(capturedImage: $capturedImage, isCapturing: $isCapturing, bleReceivedValue: $bleReceivedValue, bleManager:$bleManager)
+                    }
+
+                    Spacer()
+                }
             }
-            .onDisappear {
-                stopBLEScanTimer()  // Stop the timer when the view disappears
-            }
+            .navigationBarTitle("MEMORIES Scanner", displayMode: .inline)
+            .navigationBarItems(trailing:
+                    // First Menu (Connect/Disconnect)
+                                Menu {
+                                    ScrollView {
+                                        VStack {
+                                            // First Button (Connect/Disconnect)
+                                            Button(action: {
+                                                bleManager.isConnected ? bleManager.disconnect() : bleManager.connect()
+                                            }) {
+                                                Text(bleManager.isConnected ? "Disconnect" : "Connect")
+                                                    .frame(maxWidth: .infinity)
+                                                    .font(.headline.weight(.semibold))
+                                            }
+                                            .padding()
+                                            .background(bleManager.isConnected ? Color.red : Color.blue)
+                                            .foregroundColor(.white)
+                                            .cornerRadius(8)
+                                            
+                                            // Second Button with NavigationLink to BleTestView
+                                            NavigationLink(destination: BleTestView(bleManager: $bleManager)) {
+                                                Button(action: {
+                                                    // Action if needed when the button is tapped
+                                                }) {
+                                                    Text("Go to BleTestView")
+                                                        .frame(maxWidth: .infinity)
+                                                        .font(.headline.weight(.semibold))
+                                                }
+                                            }
+            
+
+                                        }
+                                    }
+                                } label: {
+                                    Image(systemName: "ellipsis")
+                                        .font(.title2)
+                                        .foregroundColor(.gray)
+                                }
+
+                    
+                    // Second Menu (Navigate to BleTestView)
+            )
             .alert(isPresented: $isShowingAlert) {
                 Alert(title: Text("Scanner is not connected"), message: Text("Please connect to the scanner before capturing."), dismissButton: .default(Text("OK")))
             }
+            
+            // NavigationLink to BleTestView
+        }
+        .onAppear {
+            // Optionally start BLE scan timer here if needed
+            startBLEScanTimer()
+        }
+        .onDisappear {
+            // Stop the BLE scan timer when the view disappears
+            stopBLEScanTimer()
+        }
     }
     
     // Start the BLE scan timer
